c146153131a2127046b603eff03321c68f2ae28d
Sanggul Rotua Pakpahan <114811314+SanggulRotuaPakpahan@users.noreply.github.com>
2024-12-07 03:56:31 +0700

    Update server_old.js

--- server_old.js
+++ server_old.js
@@ -149,7 +149,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
