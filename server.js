fd35034fa5af26e95cb5fc9b4de584ef1fb9f4f8
Sanggul Rotua Pakpahan <114811314+SanggulRotuaPakpahan@users.noreply.github.com>
2024-12-07 04:11:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/predict/histories", historyRoute);
 // Error handling middleware
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
